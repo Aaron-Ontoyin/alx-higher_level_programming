387eef597eba8996161496a7f331154c65dc9e57
Aaron-Ontoyin <aarononto909@gmail.com>
2023-03-08 16:54:41 -0800

    corrected 0 char p

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uppercase(s):
     count = 1
-    for c in s:
+    for c in range(len(s)):
         end = '\n' if count == len(s) else ''
-        if ord('a') <= ord(c) <= ord('z'):
-            print("{}".format(chr(ord(c) - 32)), end=end)
+        if ord('a') <= ord(s[c]) <= ord('z'):
+            print("{}".format(chr(ord(s[c]) - 32)), end=end)
         else:
-            print("{}".format(c), end=end)
+            print("{}".format(s[c]), end=end)
         count += 1
