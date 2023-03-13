aa7d7be866b6ce587f50b5d1ea352b6db9eb7920
Aaron-Ontoyin <aarononto909@gmail.com>
2023-03-13 14:37:27 -0700

    Fixed errors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,3 +8,4 @@ def print_matrix_integer(matrix=[[]]):
                     print("{:d}".format(j), end=' ')
                 else:
                     print("{:d}".format(j), end='\n')
+        print('\n')
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    l = len(sentence)
     first_char = sentence[0] if len(sentence) > 0 else None
-    return (l, first_char)
+    return (len(sentence), first_char)
