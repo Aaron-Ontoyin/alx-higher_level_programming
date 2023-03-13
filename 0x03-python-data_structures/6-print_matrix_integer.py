1f79a98294b991d9d0127c8d3bd6000ff5a844c0
Aaron-Ontoyin <aarononto909@gmail.com>
2023-03-13 14:47:51 -0700

    .

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for i in matrix:
-            for cnt, j in enumerate(i):
-                if cnt + 1 != len(i):
-                    print("{:d}".format(j), end=' ')
-                else:
-                    print("{:d}".format(j), end='\n')
-        print('\n')
+    for i in matrix:
+        print(' '.join("{:d}".format(j) for j in i))
