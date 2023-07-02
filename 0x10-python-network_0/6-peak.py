9e2c42e55fc1338a721bfd60dfd86b8b5ee8d573
Aaron-Ontoyin <aarononto90@gmail.com>
2023-07-02 18:14:33 +0000

    diff algo

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,34 +10,20 @@ def find_peak(list_of_integers):
     Returns:
         Peak of the list_of_integers(int) or None if the list is empty.
     """
-    if list_of_integers is None:
-        return None
-
     list_len = len(list_of_integers)
+
     if list_len == 0:
         return None
 
-    if list_len == 1:
-        return list_of_integers[0]
+    left = 0
+    right = list_len - 1
 
-    mid_idx = list_len // 2
+    while left < right:
+        mid = (left + right) // 2
 
-    if mid_idx != list_len - 1:
-        mid_val = list_of_integers[mid_idx]
-        left_val = list_of_integers[mid_idx - 1]
-        right_val = list_of_integers[mid_idx + 1] 
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
 
-        if left_val < mid_val and right_val < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-    else:
-        mid_val = list_of_integers[mid_idx]
-        left_val = list_of_integers[mid_idx - 1]
-
-        return (mid_val if left_val < mid_val else left_val)
-
-    if left_val > mid_val:
-        sublist = list_of_integers[0:mid_idx]
-    else:
-        sublist = list_of_integers[mid_idx + 1:]
-
-    return find_peak(sublist)
+    return list_of_integers[left]
